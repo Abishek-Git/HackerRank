6862718bd6ef6deaded3d7bf344f8fa92cd94ba2
Abisheak S <76975096+Abishek-Git@users.noreply.github.com>
2021-08-19 20:53:01 +0530

    Update Capitalize-python.py

--- Capitalize-HackerRank/Capitalize-python.py
+++ Capitalize-HackerRank/Capitalize-python.py
@@ -1,4 +1,7 @@
+#https://www.hackerrank.com/challenges/capitalize/problem
+
 """
+        challenges capitalize problem HackerRank
 You are asked to ensure that the first and last names of
 people begin with a capital letter in their passports.
 For example, alison heck should be capitalised correctly as Alison Heck.
