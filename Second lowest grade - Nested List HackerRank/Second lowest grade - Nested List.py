9029b76b947eda5c32cf6a122ffe890da319588c
Abisheak S <76975096+Abishek-Git@users.noreply.github.com>
2021-08-19 12:04:57 +0530

    Add files via upload

---
+++ Second lowest grade - Nested List HackerRank/Second lowest grade - Nested List.py
@@ -0,0 +1,32 @@
+"""
+Given the names and grades for each student in a class of N students, 
+store them in a nested list and print the name(s) of any student(s) 
+having the second lowest grade.
+
+Note: If there are multiple students with the second lowest grade, 
+order their names alphabetically and print each name on a new line.
+
+
+"""
+
+
+total = int(input())
+data = []
+points = []
+for i in range(total):
+    name = input()
+    grade = float(input())
+    data += [[name, grade]]
+    points.append(grade)
+
+points.sort()
+
+temp = 0
+for i in points:
+    if(float(i) > float(points[0])):
+        temp = float(i)
+        break
+
+for item in data:
+    if(float(item[1]) == temp):
+        print(item[0])
