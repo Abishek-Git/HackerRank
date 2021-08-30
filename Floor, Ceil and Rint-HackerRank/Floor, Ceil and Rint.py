7cc4b8829e301faa5e436c60f00034e0bf844ee2
Abisheak S <76975096+Abishek-Git@users.noreply.github.com>
2021-08-30 18:13:32 +0530

    Update Floor, Ceil and Rint.py

--- Floor, Ceil and Rint-HackerRank/Floor, Ceil and Rint.py
+++ Floor, Ceil and Rint-HackerRank/Floor, Ceil and Rint.py
@@ -1,5 +1,54 @@
+"""
+    https://www.hackerrank.com/challenges/floor-ceil-and-rint/
+        HACKERRANK
+    Practice > Python > Numpy > Floor, Ceil and Rint
+
+------------------Floor, Ceil and Rint---------------------
+
+floor
+
+The tool floor returns the floor of the input element-wise.
+
+import numpy
+
+my_array = numpy.array([1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9])
+print numpy.floor(my_array)         #[ 1.  2.  3.  4.  5.  6.  7.  8.  9.]
+
+ceil
+
+The tool ceil returns the ceiling of the input element-wise.
+
+import numpy
+
+my_array = numpy.array([1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9])
+print numpy.ceil(my_array)          #[  2.   3.   4.   5.   6.   7.   8.   9.  10.]
+
+rint
+
+The rint tool rounds to the nearest integer of input element-wise.
+
+import numpy
+
+my_array = numpy.array([1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9])
+print numpy.rint(my_array)          #[  1.   2.   3.   4.   6.   7.   8.   9.  10.]
+
+Sample Input
+
+1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8 9.9
+
+
+Sample Output
+
+[ 1.  2.  3.  4.  5.  6.  7.  8.  9.]
+[  2.   3.   4.   5.   6.   7.   8.   9.  10.]
+[  1.   2.   3.   4.   6.   7.   8.   9.  10.]
+
+"""
+
 import numpy
 numpy.set_printoptions(legacy = '1.13')
-N, M = map(int, input().split())
+a = numpy.array(list(map(float, input().split())))
 
-print(numpy.eye(N,M,k = 0))
+print(numpy.floor(a))
+print(numpy.ceil(a))
+print(numpy.rint(a))
